rishabh-d-dave/ceph

qa/cephfs: add and use get_ceph_cmd_result()
To run a command and get its return value, instead of typing something
as long as "self.mds_cluster.mon_manager.raw_cluster_cmd_result" add a
hepler method in CephFSTestCase and use it. This makes this task very
simple - "self.get_ceph_cmd_result()".

Also, remove method CephFSTestCase.run_cluster_cmd_result() in favour of
this new method.

Signed-off-by: Rishabh Dave <ridave@redhat.com>

qa/tasks/cephfs/cephfs_test_case.py
@@ -77,6 +77,13 @@ class RunCephCmd:
                 admin_remote, ctx=self.ctx,
                 logger=log.getChild('ceph_manager'))
 
+    def get_ceph_cmd_result(self, *args, **kwargs):
+        if kwargs.get('args') is None and args:
+            if len(args) == 1:
+                args = args[0]
+            kwargs['args'] = args
+        return self.cephman.run_cluster_cmd(**kwargs).exitstatus
+
 
 class CephFSTestCase(CephTestCase, RunCephCmd):
     """
@@ -196,11 +203,11 @@ class CephFSTestCase(CephTestCase, RunCephCmd):
                        'osd', f'allow rw tag cephfs data={self.fs.name}',
                        'mds', 'allow']
 
-                if self.run_cluster_cmd_result(cmd) == 0:
+                if self.get_ceph_cmd_result(*cmd) == 0:
                     break
 
                 cmd[1] = 'add'
-                if self.run_cluster_cmd_result(cmd) != 0:
+                if self.get_ceph_cmd_result(*cmd) != 0:
                     raise RuntimeError(f'Failed to create new client {cmd[2]}')
 
             # wait for ranks to become active
@@ -440,11 +447,6 @@ class CephFSTestCase(CephTestCase, RunCephCmd):
         if isinstance(cmd, str):
             cmd = shlex_split(cmd)
         return self.fs.mon_manager.raw_cluster_cmd(*cmd)
-
-    def run_cluster_cmd_result(self, cmd):
-        if isinstance(cmd, str):
-            cmd = shlex_split(cmd)
-        return self.fs.mon_manager.raw_cluster_cmd_result(*cmd)
 
     def create_client(self, client_id, moncap=None, osdcap=None, mdscap=None):
         if not (moncap or osdcap or mdscap):

roysahar/ceph-ci

(cherry picked from commit 82814ac49d31b0fc48ac4f3ec659a9f0a822acd2)

qa/tasks/cephfs/cephfs_test_case.py
@@ -55,6 +55,16 @@ class MountDetails():
         mntobj.hostfs_mntpt = self.hostfs_mntpt
 
 
+class RunCephCmd:
+
+    def get_ceph_cmd_result(self, *args, **kwargs):
+        if kwargs.get('args') is None and args:
+            if len(args) == 1:
+                args = args[0]
+            kwargs['args'] = args
+        return self.mon_manager.run_cluster_cmd(**kwargs).exitstatus
+
+
 class CephFSTestCase(CephTestCase, RunCephCmd):
     """
     Test case for Ceph FS, requires caller to populate Filesystem and Mounts,
@@ -184,11 +194,11 @@ class CephFSTestCase(CephTestCase, RunCephCmd):
                        'osd', f'allow rw tag cephfs data={self.fs.name}',
                        'mds', 'allow']
 
-                if self.run_cluster_cmd_result(cmd) == 0:
+                if self.get_ceph_cmd_result(*cmd) == 0:
                     break
 
                 cmd[1] = 'add'
-                if self.run_cluster_cmd_result(cmd) != 0:
+                if self.get_ceph_cmd_result(*cmd) != 0:
                     raise RuntimeError(f'Failed to create new client {cmd[2]}')
 
             # wait for ranks to become active
@@ -428,11 +438,6 @@ class CephFSTestCase(CephTestCase, RunCephCmd):
         if isinstance(cmd, str):
             cmd = shlex_split(cmd)
         return self.fs.mon_manager.raw_cluster_cmd(*cmd)
-
-    def run_cluster_cmd_result(self, cmd):
-        if isinstance(cmd, str):
-            cmd = shlex_split(cmd)
-        return self.fs.mon_manager.raw_cluster_cmd_result(*cmd)
 
     def create_client(self, client_id, moncap=None, osdcap=None, mdscap=None):
         if not (moncap or osdcap or mdscap):